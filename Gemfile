61cc0176e18b1a41cd62d45d277fbdddd16a0640
Etienne Bovet <ebovet@gmail.com>
2019-11-26 17:43:41 +0800

    Created an api to talk to tencent

--- Gemfile
+++ Gemfile
@@ -62,3 +62,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
+
+gem 'dotenv-rails'
+gem 'rest-client'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # [...]
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post '/login', to: 'login#login'
       resources :services, only: [:index]
     end
   end
